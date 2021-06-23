5568140cd75568eeb3cf37ac84eb5f4e2f1d09b6
Micael Souza <micael.souza@outlook.com>
2021-06-23 00:29:12 -0300

    test: ensure SignupController returns 400 if no email is provide

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,4 +17,21 @@ describe('Sugnup Controller', () => {
       new Error('Missing param: name')
     )
   })
+
+  test('Should returns 400 if no email is provide', () => {
+    const sut = new SignupController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password_123',
+        passwordConfirmation: 'any_password_123'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(
+      new Error('Missing param: email')
+    )
+  })
 })
